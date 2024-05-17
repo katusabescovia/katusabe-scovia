94b7e7bd4aefd8a670558c427e0f1976d9ea8228
katusabe scovia <scoviak588@gmail.com>
2024-05-17 16:31:25 +0300

    added katusabe scovia

---
+++ katusabe-scovia/project/form/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from. forms import*
+from . models import *
+
+# Create your views here.
+
+
+
+from django.contrib import messages
+
+def index(request):
+    if request.method == 'POST':
+        form = Userform(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Form has been submitted successfully")
+            return redirect('/')
+        else:
+            messages.error(request, "Form has errors. Please check your input.")
+    else:
+        form = Userform()
+    
+    return render(request, 'index.html', {'form': form})
